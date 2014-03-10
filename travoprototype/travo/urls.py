2348ce889870939a11c79d0716f7d91e420ea6d3
EthanWu <609734312@qq.com>
2014-03-10 15:20:14 +0800

    Revert 84b32c5..15ec099
    This rolls back to commit 84b32c569957b64cf3d55487da0c4bca70aa5423.

--- travoprototype/travo/urls.py
+++ travoprototype/travo/urls.py
@@ -4,6 +4,5 @@ from django.shortcuts import get_object_or_404, render
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^homepage', views.home_page, name='homepage')
 
 )
