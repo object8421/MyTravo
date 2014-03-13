412bba9dc0ffabc19bf9f1610b1b557693988c27
EthanWu <609734312@qq.com>
2014-03-13 16:57:51 +0800

    login and logout with session

--- Server/travo/surls.py
+++ Server/travo/surls.py
@@ -6,10 +6,11 @@ urlpatterns = patterns('travo',
 		#url(r'^user/login$', views.LoginView.as_view(), name='login'),
 		#url(r'^travel/upload$', views.UploadTravelView.as_view(), name='upload_travel')
 		url(r'^user/register$', sviews.RegisterView.as_view(), name='register'),
-		url(r'^user/login',sviews.LoginView.as_view(),name='login'),
+		url(r'^user/login$',sviews.LoginView.as_view(),name='login'),
+		url(r'^user/logout$',sviews.LogoutView.as_view(), name = 'logout'),
 		url(r'^user/show_register$', sviews.ShowRegisterView.as_view(), name='show_register'),
 		url(r'^index$', sviews.IndexView.as_view(), name ='index'),
 		url(r'^$', sviews.IndexView.as_view(), name ='index'),
-		url(r'^contact', sviews.ContactView.as_view(), name = 'contact')
+		url(r'^contact', sviews.ContactView.as_view(), name = 'contact'),
 		#url(r'^me', sviews.MyInfoView.as_view(),name = 'me')
 	)
